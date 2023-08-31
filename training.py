0c69fa2301ee5753aae8605b31914293f4412878
pedrocanddz <pedrocanddz07@gmail.com>
2023-08-31 12:20:49 -0300

    imagens armazenadas

---
+++ training.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import imgdhr
+
+data_dir = 'food_data'
+image_exts = ['.jpg', '.png', '.jpeg', 'bmp']
+os.listdir(data_dir)
